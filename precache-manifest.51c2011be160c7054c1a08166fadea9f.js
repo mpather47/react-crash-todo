7de0e38724f26783dc563c18edc2977c5c235dda
mpather47 <mpather47@gmail.com>
2020-08-21 20:30:13 +0200

    Updates

---
+++ precache-manifest.51c2011be160c7054c1a08166fadea9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c319d7f90174279b0de8e7a87edeb84",
+    "url": "/react-crash-todo/index.html"
+  },
+  {
+    "revision": "5aa1255232a3e9bdf588",
+    "url": "/react-crash-todo/static/css/main.0fbefffc.chunk.css"
+  },
+  {
+    "revision": "132c608a275ce14cc178",
+    "url": "/react-crash-todo/static/js/2.61742238.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-crash-todo/static/js/2.61742238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aa1255232a3e9bdf588",
+    "url": "/react-crash-todo/static/js/main.210f98e9.chunk.js"
+  },
+  {
+    "revision": "05f852e6654378afeb91",
+    "url": "/react-crash-todo/static/js/runtime-main.20e66c01.js"
+  }
+]);
